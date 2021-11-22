3d378b1672882d59185fb10e4c03b46846c42fa4
Rajan Mali <mysteriouskiller50@gmail.com>
2021-11-22 20:30:04 +0545

    added categories page

---
+++ pages/category/[slug].js
@@ -0,0 +1,51 @@
+import Head from 'next/head';
+import { useRouter } from 'next/router';
+
+// Components
+import PostCard from '../../components/common/PostCard';
+import CategoriesWidget from '../../components/common/CategoriesWidget';
+import Loader from '../../components/common/Loader';
+
+export default function CategoriesPage({ posts }) {
+  const router = useRouter();
+
+  if (router.isFallback) {
+    return <Loader />;
+  }
+
+  return (
+    <div className="container mx-auto px-10 mb-8">
+      <div className="grid grid-cols-1 lg:grid-cols-12 gap-12">
+        <div className="col-span-1 lg:col-span-8">
+          {posts.map((post, index) => (
+            <PostCard key={index} post={post.node} />
+          ))}
+        </div>
+        <div className="col-span-1 lg:col-span-4">
+          <div className="relative lg:sticky top-8">
+            <Categories />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+// Fetch data at build time
+export async function getStaticProps({ params }) {
+  const posts = await getCategoryPost(params.slug);
+
+  return {
+    props: { posts },
+  };
+}
+
+// Specify dynamic routes to pre-render pages based on data.
+// The HTML is generated at build time and will be reused on each request.
+export async function getStaticPaths() {
+  const categories = await getCategories();
+  return {
+    paths: categories.map(({ slug }) => ({ params: { slug } })),
+    fallback: true,
+  };
+}
