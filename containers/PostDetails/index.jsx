42f7f96d7f8a045721b8c5fef2606b5621643cbe
Rajan Mali <mysteriouskiller50@gmail.com>
2021-11-22 15:15:47 +0545

    added common components for post details page

--- containers/PostDetails/index.jsx
+++ containers/PostDetails/index.jsx
@@ -1,7 +1,7 @@
 // Components
 import PostDetailsCard from '../../components/common/PostDetailsCard';
 import PostWidget from '../../components/common/PostWidget';
-import Categories from '../../components/common/CategoriesWidget';
+import CategoriesWidget from '../../components/common/CategoriesWidget';
 
 const PostDetails = () => {
   return <div className="container mx-auto px-10">PostDetailsPage</div>;
