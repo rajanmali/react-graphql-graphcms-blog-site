bf9e7d35caa40bc6424c97a98d422736af0f8bdf
Rajan Mali <mysteriouskiller50@gmail.com>
2021-11-22 14:02:26 +0545

    added service for fetching categories

--- components/Categories/index.jsx
+++ components/Categories/index.jsx
@@ -1,4 +1,18 @@
+import { useState, useEffect } from 'react';
+import moment from 'moment';
+import Link from 'next/link';
+
+// Services
+import { getCategories } from '../../services';
+
 const Categories = () => {
+  const [categories, setCategories] = useState([]);
+
+  useEffect(() => {
+    getCategories().then((res) => {
+      setCategories(res);
+    });
+  }, []);
   return <div>Categories</div>;
 };
 
