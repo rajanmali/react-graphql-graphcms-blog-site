5c66a5ad095a704962d19c09292f3c4c551b6af4
Rajan Mali <mysteriouskiller50@gmail.com>
2021-11-22 13:14:06 +0545

    added basic post card comopnent

--- pages/index.js
+++ pages/index.js
@@ -5,18 +5,12 @@ import PostCard from '../components/PostCard';
 import PostWidget from '../components/PostWidget';
 import Categories from '../components/Categories';
 
-const posts = [
-  { title: 'React Testing', excerpt: 'Learn react testing' },
-  { title: 'React w/ Tailwind', excerpt: 'Learn react w/ Tailwind' },
-  {
-    title: 'React w/ styled components',
-    excerpt: 'Learn react w/ styled components',
-  },
-];
+// Services
+import { getPosts } from '../services';
 
-export default function Home() {
+export default function Home({ posts }) {
   return (
-    <div className="container mx-auto px-10 mb-8 bg-gray-300">
+    <div className="container mx-auto px-10 mb-8">
       <Head>
         <title>NextJS Blog</title>
         <link rel="icon" href="/favicon.ico" />
@@ -24,7 +18,7 @@ export default function Home() {
       <div className="grid grid-cols-1 lg:grid-cols-12 gap-12">
         <div className="lg:col-span-8 col-span-1">
           {posts.map((post) => (
-            <PostCard post={post} key={post.title} />
+            <PostCard post={post.node} key={post.title} />
           ))}
         </div>
         <div className="lg:col-span-4 col-span-1">
@@ -36,4 +30,9 @@ export default function Home() {
       </div>
     </div>
   );
+}
+
+export async function getStaticProps() {
+  const posts = (await getPosts()) ?? [];
+  return { props: { posts } };
 }
