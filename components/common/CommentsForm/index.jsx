d135d7e53275031ecf15aacd8ab13f9629dab4a6
Rajan Mali <mysteriouskiller50@gmail.com>
2021-11-22 19:29:25 +0545

    added comments form

--- components/common/CommentsForm/index.jsx
+++ components/common/CommentsForm/index.jsx
@@ -1,7 +1,106 @@
-import React from 'react';
+import { useState, useEffect, useRef } from 'react';
 
-const CommentsForm = () => {
-  return <div>CommentsForm</div>;
+const CommentsForm = ({ slug }) => {
+  const [error, setError] = useState(false);
+  const [localStorage, setLocalStorage] = useState(null);
+  const [showSuccessMessage, setShowSuccessMessage] = useState(false);
+
+  const commentEl = useRef();
+  const nameEl = useRef();
+  const emailEl = useRef();
+  const storeDataEl = useRef();
+
+  const handleCommentSubmission = (e) => {
+    e.preventDefault();
+
+    setError(false);
+
+    const { value: comment } = commentEl.current;
+    const { value: name } = nameEl.current;
+    const { value: email } = emailEl.current;
+    const { value: storeData } = storeDataEl.current;
+
+    if (!comment || !name || !email) {
+      setError(true);
+      return;
+    }
+
+    const commentObj = { name, email, comment, slug };
+
+    if (storeData) {
+      localStorage.setItem('name', name);
+      localStorage.setItem('email', email);
+    } else {
+      localStorage.removeItem('name');
+      localStorage.removeItem('email');
+    }
+  };
+
+  return (
+    <div className="bg-white shodow-lg rounded-lg p-8 pb-12 mb-8">
+      <h3 className="text-xl mb-8 font-semibold border-b pb-4">
+        Add your comment
+      </h3>
+      <div className="grid grid-cols gap-4 mb-4">
+        <textarea
+          ref={commentEl}
+          placeholder="Comment"
+          name="comment"
+          className="p-4 outline-none w-full rounded-lg focus:ring-2 focus:ring-gray-200 bg-gray-100 text-gray-700"
+        />
+      </div>
+      <div className="grid grid-cols lg:grid-cols-2  gap-4 mb-4">
+        <input
+          type="text"
+          ref={nameEl}
+          placeholder="Name"
+          name="name"
+          className="py-4 px-4 outline-none w-full rounded-lg focus:ring-2 focus:ring-gray-200 bg-gray-100 text-gray-700"
+        />
+        <input
+          type="text"
+          ref={emailEl}
+          placeholder="E-mail"
+          name="email"
+          className="py-4 px-4 outline-none w-full rounded-lg focus:ring-2 focus:ring-gray-200 bg-gray-100 text-gray-700"
+        />
+      </div>
+      <div className="grid grid-cols gap-4 mb-4">
+        <div>
+          <input
+            type="checkbox"
+            ref={storeDataEl}
+            id="storeData"
+            name="storeData"
+            value={true}
+          />
+          <label
+            htmlFor="storeData"
+            className="text-gray-500 cursor-pointer ml-2"
+          >
+            Save my e-mail and name for the next time I comment.
+          </label>
+        </div>
+      </div>
+      {error && (
+        <p className="text-xs text-red-500">All fields are required.</p>
+      )}
+      <div className="mt-8">
+        <button
+          type="button"
+          onClick={handleCommentSubmission}
+          className="transition duration-500 ease hover:bg-purple-500 inline-block bg-purple-400 text-lg rounded-full text-white px-8 py-3 cursor-pointer"
+        >
+          Post Comment
+        </button>
+        {showSuccessMessage && (
+          <span className="text-xl float-right font-semibold mt-3 text-green-400">
+            Comment submitted for review.
+          </span>
+        )}
+      </div>
+    </div>
+  );
 };
 
 export default CommentsForm;
